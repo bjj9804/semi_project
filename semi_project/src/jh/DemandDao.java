bd3cd099a950356903411d8698072d3026b7bc1f
JHTA <JHTA@JHTA-PC>
2018-08-24 14:56:39 +0900

    jh

--- semi_project/src/jh/DemandDao.java
+++ semi_project/src/jh/DemandDao.java
@@ -18,7 +18,7 @@ public class DemandDao {
 		return instance;
 	}
 	
-	public int getCartNum(String email) {//장바구니 번호 얻어오기
+	public int[] getCartNum(String email) {//장바구니 번호 얻어오기
 		Connection con=null;
 		PreparedStatement pstmt=null;
 		PreparedStatement pstmt1=null;
@@ -30,26 +30,31 @@ public class DemandDao {
 			pstmt=con.prepareStatement(sql);
 			pstmt.setString(1, email);
 			rs=pstmt.executeQuery();
+			int check=1;//장바구니정보가 있다.
+			int cartNum=0;
 			if(rs.next()) {//pay테이블에 해당email을 가진 행이 존재하면 일단 그 값을 꺼낸다. 없으면 일단 0을 받아온다.
-				int cartNum=rs.getInt("cartNum");
+						/////cartNum이 음수면 장바구니정보가 있는것! 음수가 아니면 장바구니정보가 없는것!
+				cartNum=rs.getInt("cartNum");				
 				if(cartNum>=0) {//만약 값이 0이라면 아예 장바구니고 주문이고 아무것도 없었다는 것. 
 								//만약 값이 양수라면 장바구니는 비었지만 과거에 주문은 이루어졌었다는것
-					String sql1="select nvl(min(orderNum),-1) as cartNum from pay";
+					check=0;//장바구니정보가 없다.
+					String sql1="select nvl(min(orderNum),-1) as cartNum from pay";//장바구니값으로 갖을 값 꺼내기
 					pstmt1=con.prepareStatement(sql1);
 					rs1=pstmt1.executeQuery();
 					if(rs1.next()) {
 						cartNum=rs1.getInt("cartNum");
-						if(cartNum>0) {
+						if(cartNum>0) {//buy테이블에 아예 장바구니정보가 없으면||초기에 장바구니담기
 							cartNum=-1;
 						}
 					}
 				}
-				return cartNum-1;
+				int[] cartPayTbCh= {check,cartNum};
+				return cartPayTbCh;
 			}			
-			return 0;
+			return null;
 		}catch(Exception e) {
 			e.printStackTrace();
-			return 0;
+			return null;
 		}finally {
 			try {
 				if(rs1!=null) rs1.close();
@@ -94,8 +99,34 @@ public class DemandDao {
 			}
 		}
 	}	
-	
-	public int cart(BuyVo bvo,PayVo pvo) {
+	public int getPrice(int buyNum) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			con=DBConnection.getConnection();
+			String sql="select price from buy where buyNum=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, buyNum);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt("price");
+			}
+			return -1;			
+		}catch(Exception e) {
+			e.printStackTrace();
+			return -1;
+		}finally {
+			try {				
+				if(rs!=null) rs.close();
+				if(pstmt!=null) pstmt.close();
+				if(con!=null) con.close();				
+			}catch(SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+	public int cart(PayVo pvo,BuyVo bvo) {//굳이 동시에 insert시키는 이유는 둘다 동시에 생성해야하니까!
 		Connection con=null;
 		PreparedStatement pstmt=null;
 		PreparedStatement pstmt1=null;
@@ -106,7 +137,7 @@ public class DemandDao {
 			pstmt1=con.prepareStatement(sql1);
 			pstmt1.setInt(1, pvo.getOrderNum());			
 			pstmt1.setString(2, pvo.getEmail());			
-			pstmt1.executeUpdate();		
+			int num=pstmt1.executeUpdate();		
 			
 			String sql="insert into buy values(buy_seq.nextval,?,?,?,?,?)";
 			pstmt=con.prepareStatement(sql);
@@ -115,11 +146,11 @@ public class DemandDao {
 			pstmt.setString(3, bvo.getIsize());
 			pstmt.setInt(4, bvo.getOrderAmount());
 			pstmt.setInt(5, bvo.getPrice());
-			pstmt.executeUpdate();
+			num+=pstmt.executeUpdate();
 			
 			con.commit();
 			
-			return 1;
+			return num;
 		}catch(Exception e) {
 			e.printStackTrace();
 			try {
@@ -138,9 +169,96 @@ public class DemandDao {
 			}
 		}
 	}
-	
-	public int order(BuyVo bvo,PayVo pvo) {
+	public int buyInsert(BuyVo bvo) {//장바구니가 이미 생성되어있는상태에서 상품을 추가할때!
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		try {
+			con=DBConnection.getConnection();
+			String sql="insert into buy values(buy_seq.nextval,?,?,?,?,?)";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, bvo.getOrderNum());
+			pstmt.setString(2, bvo.getCode());
+			pstmt.setString(3, bvo.getIsize());
+			pstmt.setInt(4, bvo.getOrderAmount());
+			pstmt.setInt(5, bvo.getPrice());			
+			
+			return pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return -1;
+		}finally {
+			try {
+				if(pstmt!=null) pstmt.close();
+				if(con!=null) con.close();				
+			}catch(SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+	public int buyUpdate(int buyNum,int orderNum) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		try {
+			con=DBConnection.getConnection();
+			String sql="update buy set orderNum=? where buyNum=?";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, orderNum);
+			pstmt.setInt(2, buyNum);
+			return pstmt.executeUpdate();			
+		}catch(Exception e) {
+			e.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return -1;
+		}finally {
+			try {
+				if(pstmt!=null) pstmt.close();
+				if(con!=null) con.close();				
+			}catch(SQLException se) {
+				se.printStackTrace();
+			}
+		}
 		
+	}
+	public int payInsert(PayVo pvo) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		try {
+			con=DBConnection.getConnection();
+			con.setAutoCommit(false);
+			String sql1="insert into pay values(?,sysdate,'배송준비중',?,?,?,?,?)";
+			pstmt=con.prepareStatement(sql1);
+			pstmt.setInt(1, pvo.getOrderNum());
+			pstmt.setString(2, pvo.getMethod());
+			pstmt.setString(3, pvo.getAddr());
+			pstmt.setString(4, pvo.getEmail());
+			pstmt.setInt(5, pvo.getTotalPrice());
+			pstmt.setInt(6, pvo.getPayMoney());
+			return pstmt.executeUpdate();			
+		}catch(Exception e) {
+			e.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return -1;
+		}finally {
+			try {
+				if(pstmt!=null) pstmt.close();
+				if(con!=null) con.close();				
+			}catch(SQLException se) {
+				se.printStackTrace();
+			}
+		}
 	}
 	
 }
