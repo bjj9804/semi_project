4faa0b4fb0129c4a0b112d441838dfcfb3053e40
rkdxhdvk <rkdxhdvk@naver.com>
2018-08-29 18:53:30 +0900

    cc

--- semi_project/src/ms/ReviewBoardController.java
+++ semi_project/src/ms/ReviewBoardController.java
@@ -19,7 +19,6 @@ import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import mh.UsersDao;
 import mh.UsersVo;
 
-
 @WebServlet("/reviewBoard.do")
 public class ReviewBoardController extends HttpServlet {
 	@Override
@@ -33,35 +32,32 @@ public class ReviewBoardController extends HttpServlet {
 			list(request, response);
 		} else if (cmd != null && cmd.equals("detail")) {
 			detail(request, response);
-		}else if (cmd != null && cmd.equals("searchlist")) {
+		} else if (cmd != null && cmd.equals("searchlist")) {
 			searchlist(request, response);
-		}else if (cmd != null && cmd.equals("update")) {
+		} else if (cmd != null && cmd.equals("update")) {
 			update(request, response);
-		}else if (cmd != null && cmd.equals("delete")) {
+		} else if (cmd != null && cmd.equals("delete")) {
 			delete(request, response);
 		}
 	}
 
-	/*private void insert(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("email");
-		UsersDao udao = UsersDao.getInstance();
-		UsersVo uvo = udao.select(email);
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		int height = Integer.parseInt(request.getParameter("height"));
-		int weight = Integer.parseInt(request.getParameter("weight"));
-		String img = request.getParameter("img");
-		System.out.println(email + title + content + height + weight + img);
-		ReviewBoardVo vo = new ReviewBoardVo(0, uvo.getName(), email, title, content, height, weight, 0, null, img);
-		ReviewBoardDao dao = ReviewBoardDao.getInstance();
-		int n = dao.insert(vo);
-		if (n > 0) {
-			list(request, response);
-		}
-	}*/
-	
-	public void insert(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	/*
+	 * private void insert(HttpServletRequest request, HttpServletResponse response)
+	 * throws ServletException, IOException { String email =
+	 * request.getParameter("email"); UsersDao udao = UsersDao.getInstance();
+	 * UsersVo uvo = udao.select(email); String title =
+	 * request.getParameter("title"); String content =
+	 * request.getParameter("content"); int height =
+	 * Integer.parseInt(request.getParameter("height")); int weight =
+	 * Integer.parseInt(request.getParameter("weight")); String img =
+	 * request.getParameter("img"); System.out.println(email + title + content +
+	 * height + weight + img); ReviewBoardVo vo = new ReviewBoardVo(0,
+	 * uvo.getName(), email, title, content, height, weight, 0, null, img);
+	 * ReviewBoardDao dao = ReviewBoardDao.getInstance(); int n = dao.insert(vo); if
+	 * (n > 0) { list(request, response); } }
+	 */
+
+	public void insert(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ServletContext context = getServletContext(); // 어플리케이션에 대한 정보를 ServletContext 객체가 갖음
 		String saveDir = context.getRealPath("Upload"); // 절대경로를 가져옴
 		System.out.println("절대경로 >> " + saveDir);
@@ -85,11 +81,12 @@ public class ReviewBoardController extends HttpServlet {
 			ReviewBoardDao dao = ReviewBoardDao.getInstance();
 			ItemImgVo ivo = dao.getItemImg(code);
 			String itemImg = ivo.getImgScr();
-			ReviewBoardVo vo = new ReviewBoardVo(0, uvo.getName(), email, title, content, height, weight, 0, null, img, itemImg, code);
+			ReviewBoardVo vo = new ReviewBoardVo(0, uvo.getName(), email, title, content, height, weight, 0, null, img,
+					itemImg, code);
 			int n = dao.insert(vo);
 			if (n > 0) {
 				list(request, response);
-			}else {
+			} else {
 				System.out.println("안됨");
 			}
 		}
@@ -118,15 +115,15 @@ public class ReviewBoardController extends HttpServlet {
 			endPage = pageCount;
 		}
 		String email = request.getParameter("email");
-		int flag=0;
-		if(!email.equals("")) {//로그인을 하고 들어온 경우
-			UsersDao usersDao=UsersDao.getInstance();
-			UsersVo vo=usersDao.select(email);
-			flag=vo.getFlag();//관리자인지 회원인지			
-		}else {//로그인을 안하고 들어온경우
-			flag=1;
+		int flag = 0;
+		if (!email.equals("")) {// 로그인을 하고 들어온 경우
+			UsersDao usersDao = UsersDao.getInstance();
+			UsersVo vo = usersDao.select(email);
+			flag = vo.getFlag();// 관리자인지 회원인지
+		} else {// 로그인을 안하고 들어온경우
+			flag = 1;
 		}
-		request.setAttribute("flag", flag);	
+		request.setAttribute("flag", flag);
 		request.setAttribute("list", list);
 		request.setAttribute("pageCount", pageCount);
 		request.setAttribute("startPage", startPage);
@@ -134,20 +131,23 @@ public class ReviewBoardController extends HttpServlet {
 		request.setAttribute("pageNum", pageNum);
 		request.getRequestDispatcher("/board/review_list.jsp").forward(request, response);
 	}
-	
-	public void searchlist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	public void searchlist(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		int height = 0;
 		int weight = 0;
-		if(Integer.parseInt(request.getParameter("height"))!=0 || Integer.parseInt(request.getParameter("weight"))!=0){
+		if (Integer.parseInt(request.getParameter("height")) != 0
+				|| Integer.parseInt(request.getParameter("weight")) != 0) {
 			height = Integer.parseInt(request.getParameter("height"));
 			weight = Integer.parseInt(request.getParameter("weight"));
 			request.getSession().setAttribute("height", height);
 			request.getSession().setAttribute("weight", weight);
-		}else if(Integer.parseInt(request.getParameter("height"))!=0 || Integer.parseInt(request.getParameter("height"))!=0){
-			height = (Integer)request.getSession().getAttribute("height");
-			weight = (Integer)request.getSession().getAttribute("weight");
+		} else if (Integer.parseInt(request.getParameter("height")) != 0
+				|| Integer.parseInt(request.getParameter("height")) != 0) {
+			height = (Integer) request.getSession().getAttribute("height");
+			weight = (Integer) request.getSession().getAttribute("weight");
 		}
-		System.out.println(height  + " " + weight);
+		System.out.println(height + " " + weight);
 		String spageNum = request.getParameter("pageNum");
 		int pageNum = 1;
 		if (spageNum != null) {
@@ -158,10 +158,10 @@ public class ReviewBoardController extends HttpServlet {
 		ReviewBoardDao dao = ReviewBoardDao.getInstance();
 		ArrayList<ReviewBoardVo> list = dao.searchlist(startRow, endRow, height, weight);
 		int pageCount = 0;
-		if(height!=0 || weight!=0) {
-			pageCount = (int)Math.ceil(dao.getSearchCount(height, weight)/10.0);
-		}else {
-			pageCount = (int)Math.ceil(dao.getCount()/10.0);
+		if (height != 0 || weight != 0) {
+			pageCount = (int) Math.ceil(dao.getSearchCount(height, weight) / 10.0);
+		} else {
+			pageCount = (int) Math.ceil(dao.getCount() / 10.0);
 		}
 		int startPage = ((pageNum - 1) / 10 * 10) + 1;
 		int endPage = startPage + 9;
@@ -180,50 +180,54 @@ public class ReviewBoardController extends HttpServlet {
 		int num = Integer.parseInt(request.getParameter("num"));
 		String cmd1 = request.getParameter("cmd1");
 		String cmd2 = request.getParameter("cmd2");
-		int flag=Integer.parseInt(request.getParameter("flag"));
+		int flag = Integer.parseInt(request.getParameter("flag"));
 		ReviewBoardDao dao = ReviewBoardDao.getInstance();
-		int n=dao.hitup(num);
+		int n = dao.hitup(num);
 		ReviewBoardVo vo = dao.detail(num);
 		request.setAttribute("vo", vo);
 		request.setAttribute("flag", flag);
 		request.setAttribute("pageNum", request.getAttribute("pageNum"));
 		request.setAttribute("cmd2", cmd2);
-		if(cmd1.equals("update")) {
+		if (cmd1.equals("update")) {
 			request.getRequestDispatcher("/board/review_update.jsp").forward(request, response);
-		}else {
+		} else {
 			request.getRequestDispatcher("/board/review_detail.jsp").forward(request, response);
 		}
 	}
-	
+
 	public void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email=request.getParameter("email");
-		String pageNum=request.getParameter("pageNum");
-		String checkList=request.getParameter("checkList");
-		String[] checkArray=checkList.split(",");
+		ServletContext context = getServletContext();
+		String saveDir = context.getRealPath("Upload");
+		String email = request.getParameter("email");
+		String pageNum = request.getParameter("pageNum");
+		String checkList = request.getParameter("checkList");
+		String[] checkArray = checkList.split(",");
 		String cmd2 = request.getParameter("cmd2");
 		ReviewBoardDao dao = ReviewBoardDao.getInstance();
-		boolean bool=true;
-		for(int i=0;i<checkArray.length;i++) {
-			int num=Integer.parseInt(checkArray[i]);
-			if(dao.delete(num)<0) {
-				bool=false;
+		boolean bool = true;
+		for (int i = 0; i < checkArray.length; i++) {
+			int num = Integer.parseInt(checkArray[i]);
+			File f = new File(saveDir + "/" + img);
+			if (f.exists()) {
+				if (f.delete())
+					dao.delete(num);
+			}
+			if (bool == false) {
+				System.out.println("삭제 실패");
+				return;
+			}
+			request.setAttribute("email", email);
+			System.out.println(pageNum);
+			request.setAttribute("pageNum", pageNum);
+			if (cmd2.equals("myshop")) {
+				request.getRequestDispatcher("/myshopBoard.do?cmd=reviewList").forward(request, response);
+			} else {
+				request.getRequestDispatcher("/reviewBoard.do?cmd=list").forward(request, response);
 			}
 		}
-		if(bool==false) {
-			System.out.println("삭제 실패");
-			return;
-		}
-		request.setAttribute("email", email);
-		System.out.println(pageNum);
-		request.setAttribute("pageNum", pageNum);
-		if(cmd2.equals("myshop"))
-			request.getRequestDispatcher("/myshopBoard.do?cmd=reviewList").forward(request, response);
-		else
-			request.getRequestDispatcher("/reviewBoard.do?cmd=list").forward(request, response);
 	}
-	
-	public void update(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+
+	public void update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ServletContext context = getServletContext(); // 어플리케이션에 대한 정보를 ServletContext 객체가 갖음
 		String saveDir = context.getRealPath("Upload"); // 절대경로를 가져옴
 		System.out.println("절대경로 >> " + saveDir);
@@ -243,37 +247,38 @@ public class ReviewBoardController extends HttpServlet {
 			String img = mr.getFilesystemName("img");
 			String exImg = vo1.getImg();
 			String cmd2 = request.getParameter("cmd2");
+			request.setAttribute("num", num);
 			System.out.println(num + " " + title + " " + content + " " + img);
-			ReviewBoardVo vo = new ReviewBoardVo(num, vo1.getName(), vo1.getEmail(), title, content, vo1.getHeight(), vo1.getWeight(), vo1.getHit(), vo1.getRegdate(), img, vo1.getItemImg(), vo1.getCode());
+			ReviewBoardVo vo = new ReviewBoardVo(num, vo1.getName(), vo1.getEmail(), title, content, vo1.getHeight(),
+					vo1.getWeight(), vo1.getHit(), vo1.getRegdate(), img, vo1.getItemImg(), vo1.getCode());
 			int n = dao.update(vo);
 			if (n > 0) {
 				File f = new File(saveDir + "/" + exImg);
-				if(f.exists()) {
-					if(f.delete()) {
-						if(cmd2.equals("myshop"))
+				if (f.exists()) {
+					if (f.delete()) {
+						if (cmd2.equals("myshop"))
 							request.getRequestDispatcher("/myshopBoard.do?cmd=reviewList").forward(request, response);
 						else
 							list(request, response);
 					}
 				}
-			}else {
+			} else {
 				System.out.println("안됨");
 			}
 		}
 	}
-	/*public void update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ReviewBoardDao dao = ReviewBoardDao.getInstance();
-		int num = Integer.parseInt(request.getParameter("num"));
-		ReviewBoardVo vo1 = dao.detail(num);
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		String img = request.getParameter("img");
-		System.out.println(num + " " + title + " " + content + " " + img);
-		ReviewBoardVo vo = new ReviewBoardVo(num, vo1.getName(), vo1.getEmail(), title, content, vo1.getHeight(), vo1.getWeight(), vo1.getHit(), vo1.getRegdate(), img, vo1.getItemImg(), vo1.getCode());
-		int n = dao.update(vo);
-		if(n>0) {
-			list(request, response);
-		}
-	}*/
-	
+	/*
+	 * public void update(HttpServletRequest request, HttpServletResponse response)
+	 * throws ServletException, IOException { ReviewBoardDao dao =
+	 * ReviewBoardDao.getInstance(); int num =
+	 * Integer.parseInt(request.getParameter("num")); ReviewBoardVo vo1 =
+	 * dao.detail(num); String title = request.getParameter("title"); String content
+	 * = request.getParameter("content"); String img = request.getParameter("img");
+	 * System.out.println(num + " " + title + " " + content + " " + img);
+	 * ReviewBoardVo vo = new ReviewBoardVo(num, vo1.getName(), vo1.getEmail(),
+	 * title, content, vo1.getHeight(), vo1.getWeight(), vo1.getHit(),
+	 * vo1.getRegdate(), img, vo1.getItemImg(), vo1.getCode()); int n =
+	 * dao.update(vo); if(n>0) { list(request, response); } }
+	 */
+
 }
